797072ec4b8f8173d94ae4093e89c9c944a56cc2
rammanohar99 <itsmanohar99@gmail.com>
2024-07-23 19:05:33 +0530

    Add initial web scraper script

---
+++ scraper.py
@@ -0,0 +1,18 @@
+
+# scraper.py
+
+import requests
+
+from bs4 import BeautifulSoup
+
+
+
+url = 'http://example.com'
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+
+
+print(soup.title.text)
